4d988199eaad89070a026bc622469679aac0d388
multicatch <cezaryre@gmail.com>
2021-06-26 10:36:21 +0200

    Add additional benchmarks

--- src/main/java/io/github/multicatch/LoggerBenchmark.java
+++ src/main/java/io/github/multicatch/LoggerBenchmark.java
@@ -7,13 +7,17 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 import org.slf4j.impl.StaticLoggerBinder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,19 +30,37 @@ public class LoggerBenchmark {
     private static final String MESSAGE_WITH_PARAMS = "Hello there, {}, {}, {}";
     private static final String MESSAGE_WITH_PARAMS_JAVA_UTIL = "Hello there, {0}, {1}, {2}";
     private static final Object[] PARAMS = new Object[] { "Jane", 2, new DummyType() };
+    private static final Throwable THROWABLE = new IllegalStateException("Error");
+
+    private static final List<Options> BENCHMARK_CONFIGS = Arrays.asList(
+            createOptions(Mode.Throughput, TimeUnit.MILLISECONDS)
+                    .result("throughput.json")
+                    .build(),
+            createOptions(Mode.SampleTime, TimeUnit.MICROSECONDS)
+                    .result("sample-time.json")
+                    .build(),
+            createOptions(Mode.AverageTime, TimeUnit.MICROSECONDS)
+                    .result("average.json")
+                    .build()
+    );
 
     public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
+        for (Options opt : BENCHMARK_CONFIGS) {
+            new Runner(opt).run();
+        }
+    }
+
+    static ChainedOptionsBuilder createOptions(Mode mode, TimeUnit timeUnit) {
+        return new OptionsBuilder()
                 .include(LoggerBenchmark.class.getSimpleName())
-                .mode(Mode.Throughput)
-                .timeUnit(TimeUnit.MILLISECONDS)
+                .mode(mode)
+                .timeUnit(timeUnit)
                 .warmupTime(TimeValue.seconds(10))
                 .warmupIterations(5)
                 .measurementIterations(10)
                 .forks(1)
-                .build();
-
-        new Runner(opt).run();
+                .jvmArgsAppend("-Djava.lang.invoke.stringConcat=BC_SB")
+                .resultFormat(ResultFormatType.JSON);
     }
 
     @Benchmark
@@ -54,6 +76,18 @@ public class LoggerBenchmark {
     }
 
     @Benchmark
+    public void javaLoggerThrowable(BenchmarkState state) {
+        Logger javaLogger = state.getJavaLogger();
+        javaLogger.log(Level.INFO, THROWABLE, () -> SIMPLE_MESSAGE);
+    }
+
+    @Benchmark
+    public void javaLoggerDisabled(BenchmarkState state) {
+        Logger javaLogger = state.getJavaLogger();
+        javaLogger.log(Level.FINEST, MESSAGE_WITH_PARAMS_JAVA_UTIL, PARAMS);
+    }
+
+    @Benchmark
     public void logbackLogger(BenchmarkState state) {
         org.slf4j.Logger logbackLogger = state.getLogbackLogger();
         logbackLogger.info(MESSAGE_WITH_PARAMS, PARAMS);
@@ -66,6 +100,18 @@ public class LoggerBenchmark {
     }
 
     @Benchmark
+    public void logbackLoggerThrowable(BenchmarkState state) {
+        org.slf4j.Logger logbackLogger = state.getLogbackLogger();
+        logbackLogger.error(SIMPLE_MESSAGE, THROWABLE);
+    }
+
+    @Benchmark
+    public void logbackLoggerDisabled(BenchmarkState state) {
+        org.slf4j.Logger logbackLogger = state.getLogbackLogger();
+        logbackLogger.trace(MESSAGE_WITH_PARAMS, PARAMS);
+    }
+
+    @Benchmark
     public void log4jLogger(BenchmarkState state) {
         org.apache.logging.log4j.Logger log4jLogger = state.getLog4jLogger();
         log4jLogger.info(MESSAGE_WITH_PARAMS, PARAMS);
@@ -75,6 +121,18 @@ public class LoggerBenchmark {
     public void log4jLoggerWithoutParams(BenchmarkState state) {
         org.apache.logging.log4j.Logger log4jLogger = state.getLog4jLogger();
         log4jLogger.info(SIMPLE_MESSAGE);
+    }
+
+    @Benchmark
+    public void log4jLoggerThrowable(BenchmarkState state) {
+        org.apache.logging.log4j.Logger log4jLogger = state.getLog4jLogger();
+        log4jLogger.error(SIMPLE_MESSAGE, THROWABLE);
+    }
+
+    @Benchmark
+    public void log4jLoggerDisabled(BenchmarkState state) {
+        org.apache.logging.log4j.Logger log4jLogger = state.getLog4jLogger();
+        log4jLogger.trace(MESSAGE_WITH_PARAMS, PARAMS);
     }
 
     @State(Scope.Benchmark)
